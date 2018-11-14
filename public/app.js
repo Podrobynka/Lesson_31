086ebc61801c3749d7ea0d4b52a52bb714ba0427
Podrobynka <yfgurda@gmail.com>
2018-11-14 22:42:56 +0200

    display number of items for each product

--- public/app.js
+++ public/app.js
@@ -1,5 +1,4 @@
-function something()
-{
+function somethingss() {
   var x = window.localStorage.getItem('bbb');
   x = x * 1 + 1;
 
@@ -8,10 +7,22 @@ function something()
   alert(x);
 }
 
-function add_to_cart(id)
-{
+function add_to_cart(id) {
   var key = 'product_' + id;
   var x = window.localStorage.getItem(key);
   x = x * 1 + 1;
   window.localStorage.setItem(key, x);
+  update_cart();
 }
+
+function update_cart() {
+  for(var i=0, len=localStorage.length; i<len; i++) {
+    var id = localStorage.key(i);
+    var value = localStorage[id];
+    document.getElementById("in_cart_for_" + id).innerHTML = "In cart: " + value;
+  }
+}
+
+$(document).ready(function(){
+  update_cart()
+})
