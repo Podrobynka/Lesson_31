560739b01bd8f1133b05c5ce57ffeb3623e33ecc
Roman Guk <roman.guk@techmagic.co>
2018-11-17 23:31:26 +0200

    add function remove_from_cart

--- public/app.js
+++ public/app.js
@@ -15,6 +15,14 @@ function add_to_cart(id) {
   update_cart();
 }
 
+function remove_from_cart(id) {
+  var key = 'product_' + id;
+  var x = window.localStorage.getItem(key);
+  x = x * 1 - 1;
+  window.localStorage.setItem(key, x);
+  update_cart();
+}
+
 function update_cart() {
   for(var i=0, len=localStorage.length; i<len; i++) {
     var id = localStorage.key(i);
