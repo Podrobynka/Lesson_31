4fffa3168a29fe7ccf58ac756fe404d343a11d20
Podrobynka <yfgurda@gmail.com>
2018-11-18 16:32:48 +0200

    fix (document).ready in app.js

--- public/app.js
+++ public/app.js
@@ -1,26 +1,9 @@
 function something() {
-function somethings() {
   var x = window.localStorage.getItem('bbb');
   x = x * 1 + 1;
-
   window.localStorage.setItem('bbb', x);
-
   alert(x);
 }
-
-function update_cart() {
-  for(var i=0, len=localStorage.length; i<len; i++) {
-    var id = localStorage.key(i);
-    var value = localStorage[id];
-    document.getElementById("in_cart_for_" + id).innerHTML = "In cart: " + value;
-  }
-}
-
-// function update_cart() {
-//   $.each(localStorage, function(key, value){
-//     document.getElementById("in_cart_for_" + key).innerHTML = "In cart: " + value;
-//   });
-// }
 
 function add_to_cart(id) {
   var key = 'product_' + id;
@@ -38,7 +21,20 @@ function remove_from_cart(id) {
   update_cart();
 }
 
+function update_cart() {
+  for(var i=0, len=localStorage.length; i<len; i++) {
+    var id = localStorage.key(i);
+    var value = localStorage[id];
+    document.getElementById("in_cart_for_" + id).innerHTML = "In cart: " + value;
+  }
+}
+
+// function update_cart() {
+//   $.each(localStorage, function(key, value){
+//     document.getElementById("in_cart_for_" + key).innerHTML = "In cart: " + value;
+//   });
+// }
+
 $(document).ready(function(){
   update_cart()
 })
-}
